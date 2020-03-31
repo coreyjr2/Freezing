995ee228939b6c4fd058d2f661ea5e2d93aa1fd1
mmarder2 <mmarder2@illinois.edu>
2020-03-31 10:56:17 -0500

    Started Data Cleaning
    start data clean - only 3 variables, will complete before EOD

--- Freezing EFA.R
+++ Freezing EFA.R
@@ -2,3 +2,12 @@
 ### Freezing EFA Analysis ###
 ##############################
 
+freezing_raw<-read.csv(file.choose())
+
+## install relevant packages ##
+install.packages("tidyverse")
+y
+library(tidyverse)
+
+## data cleaning - removing columns ##
+freezing_raw<-select(freezing_raw,-c(redcap_survey_identifier,consent_form_timestamp,sona_id))
