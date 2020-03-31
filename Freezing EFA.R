fc5e0e420e7ab712b755989457ef331ccff64f95
mmarder2 <mmarder2@illinois.edu>
2020-03-31 16:43:57 -0500

    Clearing Consent Only IDs_NA

--- Freezing EFA.R
+++ Freezing EFA.R
@@ -13,7 +13,6 @@ library(tidyverse)
 
 ######################################
 ## data cleaning - removing columns ##
-## the following lines are separated by chunks of cleaning for accessibility##
 ######################################
 freezing_raw<-select(freezing_raw,-c(redcap_survey_identifier,consent_form_timestamp,sona_id,con_fn,con_ln,con_date,con_sig,con_email,consent_form_complete,asi_timestamp,asi_complete,pswq_timestamp,pswq_complete,rrq_timestamp,rrq_complete,sias_timestamp,sias_complete,masq_timestamp,masq_complete,lec_timestamp,lec_complete,siasii_timestamp,siasii_complete,afq_timestamp,afq_complete,afqs_timestamp,afqs_complete,panas_timestamp,panas_complete,atq_timestamp,atq_complete,brief_timestamp,brief_complete,debriefing_form_timestamp,debriefing_form_complete))
-
+freezing_raw<-freezing_raw[complete.cases(freezing_raw[ , 2]),]
