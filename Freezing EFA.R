8725fef551f6c8fef3e5d259245258f782f9b099
mmarder2 <mmarder2@illinois.edu>
2020-04-07 10:51:12 -0500

    First Attempt at Removing Dup Participants

--- Freezing EFA.R
+++ Freezing EFA.R
@@ -14,14 +14,34 @@ install.packages("psych")
 library(psych)
 install.packages("GPArotation")
 library(GPArotation)
+install.packages("qdap")
+library(qdap)
 
 ######################################
 ## data cleaning - removing columns ##
 ######################################
-freezing_raw<-select(freezing_raw,-c(redcap_survey_identifier,consent_form_timestamp,sona_id,con_fn,con_ln,con_date,con_sig,con_email,consent_form_complete,asi_timestamp,asi_complete,pswq_timestamp,pswq_complete,rrq_timestamp,rrq_complete,sias_timestamp,sias_complete,masq_timestamp,masq_complete,lec_timestamp,lec_complete,siasii_timestamp,siasii_complete,afq_timestamp,afq_complete,afqs_timestamp,afqs_complete,panas_timestamp,panas_complete,atq_timestamp,atq_complete,brief_timestamp,brief_complete,debriefing_form_timestamp,debriefing_form_complete))
+freezing_raw<-select(freezing_raw,-c(redcap_survey_identifier,consent_form_timestamp,sona_id,con_date,con_sig,con_email,consent_form_complete,asi_timestamp,asi_complete,pswq_timestamp,pswq_complete,rrq_timestamp,rrq_complete,sias_timestamp,sias_complete,masq_timestamp,masq_complete,lec_timestamp,lec_complete,siasii_timestamp,siasii_complete,afq_timestamp,afq_complete,afqs_timestamp,afqs_complete,panas_timestamp,panas_complete,atq_timestamp,atq_complete,brief_timestamp,brief_complete,debriefing_form_timestamp,debriefing_form_complete))
+freezing_raw$con_fn <- as.character(freezing_raw$con_fn)
+freezing_raw$con_ln <- as.character(freezing_raw$con_ln)
+
+### Make sure conversion worked - diagnostic steps ###
+typeof(freezing_raw$con_fn)
+typeof(freezing_raw$con_ln)
+str(freezing_raw$con_fn)
+str(freezing_raw$con_ln)
+
 
 ## specifying column index 390 = exclusion of any incomplete data ##
 freezing_raw<-freezing_raw[complete.cases(freezing_raw[ , 390]),]
+
+
+### extract repeat participants ###
+duplicates(freezing_raw$con_fn)
+duplicates(freezing_raw$con_ln)
+grepl()
+t2_raw<-freezing_raw %>%
+  filter(con_fn==grepl(con_fn))
+
 
 ## cutting out AFQ endorsement section ##
 freezing_raw<-select(freezing_raw,-c(afq_1:afq_24))
