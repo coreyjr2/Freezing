41eda5b15b68b18538b2dc2d73973c73b95ef3ab
coreyjr2 <56316889+coreyjr2@users.noreply.github.com>
2020-04-08 12:13:11 -0500

    Made some cosmetic changes

--- Freezing EFA.R
+++ Freezing EFA.R
@@ -20,31 +20,41 @@ library(qdap)
 ######################################
 ## data cleaning - removing columns ##
 ######################################
-freezing_raw<-select(freezing_raw,-c(redcap_survey_identifier,consent_form_timestamp,sona_id,con_date,con_sig,con_email,consent_form_complete,asi_timestamp,asi_complete,pswq_timestamp,pswq_complete,rrq_timestamp,rrq_complete,sias_timestamp,sias_complete,masq_timestamp,masq_complete,lec_timestamp,lec_complete,siasii_timestamp,siasii_complete,afq_timestamp,afq_complete,afqs_timestamp,afqs_complete,panas_timestamp,panas_complete,atq_timestamp,atq_complete,brief_timestamp,brief_complete,debriefing_form_timestamp,debriefing_form_complete))
+
+## Remove unneeded columns from the dataset, refer to data dictionary for more info 
+freezing_raw<-dplyr::select(freezing_raw,-c(redcap_survey_identifier,consent_form_timestamp,sona_id,con_date,con_sig,con_email,consent_form_complete,asi_timestamp,asi_complete,pswq_timestamp,pswq_complete,rrq_timestamp,rrq_complete,sias_timestamp,sias_complete,masq_timestamp,masq_complete,lec_timestamp,lec_complete,siasii_timestamp,siasii_complete,afq_timestamp,afq_complete,afqs_timestamp,afqs_complete,panas_timestamp,panas_complete,atq_timestamp,atq_complete,brief_timestamp,brief_complete,debriefing_form_timestamp,debriefing_form_complete))
+
+## Convert first and last name columns to characters
 freezing_raw$con_fn <- as.character(freezing_raw$con_fn)
 freezing_raw$con_ln <- as.character(freezing_raw$con_ln)
 
 ### Make sure conversion worked - diagnostic steps ###
+## examine the class of first and last name columns 
 typeof(freezing_raw$con_fn)
 typeof(freezing_raw$con_ln)
+## examine the structure of first and last name columns 
 str(freezing_raw$con_fn)
 str(freezing_raw$con_ln)
 
 
 ## specifying column index 390 = exclusion of any incomplete data ##
 freezing_raw<-freezing_raw[complete.cases(freezing_raw[ , 390]),]
+#Column 390 is the last item of data collection, if the participant did not make it that far in the survey, they did not complete all measures, and they are excluded with this line of code 
 
 
 ### extract repeat participants ###
 duplicates(freezing_raw$con_fn)
 duplicates(freezing_raw$con_ln)
 grepl()
+
+#Generate a new data set for Time 2
 t2_raw<-freezing_raw %>%
-  filter(con_fn==grepl(con_fn))
+  dplyr::filter(con_fn==grepl(con_fn))
 
 
 ## cutting out AFQ endorsement section ##
-freezing_raw<-select(freezing_raw,-c(afq_1:afq_24))
+freezing_raw<-dplyr::select(freezing_raw,-c(afq_1:afq_24))
+#Check to see if we only have complete cases in the raw dataset
 complete.cases(freezing_raw)                     
 
 ###################################################
