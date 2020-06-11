7fef89915ae5c45916d4cf89d507c3f35c747b99
coreyjr2 <56316889+coreyjr2@users.noreply.github.com>
2020-06-11 11:32:34 -0500

    Update Freezing EFA.R

--- Freezing EFA.R
+++ Freezing EFA.R
@@ -2082,3 +2082,415 @@ save(dep_plot, file = "dep_plot.RData")
 save(freezing_raw_d_age, file = "freezing_raw_d_age.RData")
 save(T1_dup, file = "T1_dup.RData")
 
+
+
+
+
+#################################################################
+###### K- Means Clustering for Determining Item quality #########
+#################################################################
+
+afq_1<-as.data.frame(cor(afq[, c("afqs_1", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_2<-as.data.frame(cor(afq[, c("afqs_2", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_3<-as.data.frame(cor(afq[, c("afqs_3", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_4<-as.data.frame(cor(afq[, c("afqs_4", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_5<-as.data.frame(cor(afq[, c("afqs_5", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_6<-as.data.frame(cor(afq[, c("afqs_6", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_7<-as.data.frame(cor(afq[, c("afqs_7", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_8<-as.data.frame(cor(afq[, c("afqs_8", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_9<-as.data.frame(cor(afq[, c("afqs_9", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_10<-as.data.frame(cor(afq[, c("afqs_10", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_11<-as.data.frame(cor(afq[, c("afqs_11", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_12<-as.data.frame(cor(afq[, c("afqs_12", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_13<-as.data.frame(cor(afq[, c("afqs_13", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_14<-as.data.frame(cor(afq[, c("afqs_14", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_15<-as.data.frame(cor(afq[, c("afqs_15", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_16<-as.data.frame(cor(afq[, c("afqs_16", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_17<-as.data.frame(cor(afq[, c("afqs_17", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_18<-as.data.frame(cor(afq[, c("afqs_18", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_19<-as.data.frame(cor(afq[, c("afqs_19", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_20<-as.data.frame(cor(afq[, c("afqs_20", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_21<-as.data.frame(cor(afq[, c("afqs_21", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_22<-as.data.frame(cor(afq[, c("afqs_22", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_23<-as.data.frame(cor(afq[, c("afqs_23", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_24<-as.data.frame(cor(afq[, c("afqs_24", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_25<-as.data.frame(cor(afq[, c("afqs_25", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_26<-as.data.frame(cor(afq[, c("afqs_26", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_27<-as.data.frame(cor(afq[, c("afqs_27", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_28<-as.data.frame(cor(afq[, c("afqs_28", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_29<-as.data.frame(cor(afq[, c("afqs_30", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_30<-as.data.frame(cor(afq[, c("afqs_31", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_31<-as.data.frame(cor(afq[, c("afqs_32", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_32<-as.data.frame(cor(afq[, c("afqs_33", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_33<-as.data.frame(cor(afq[, c("afqs_34", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_34<-as.data.frame(cor(afq[, c("afqs_35", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_35<-as.data.frame(cor(afq[, c("afqs_36", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_36<-as.data.frame(cor(afq[, c("afqs_37", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_37<-as.data.frame(cor(afq[, c("afqs_38", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_38<-as.data.frame(cor(afq[, c("afqs_39", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_39<-as.data.frame(cor(afq[, c("afqs_40", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_40<-as.data.frame(cor(afq[, c("afqs_41", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_41<-as.data.frame(cor(afq[, c("afqs_42", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_42<-as.data.frame(cor(afq[, c("afqs_43", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_43<-as.data.frame(cor(afq[, c("afqs_44", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_44<-as.data.frame(cor(afq[, c("afqs_45", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_45<-as.data.frame(cor(afq[, c("afqs_46", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_46<-as.data.frame(cor(afq[, c("afqs_47", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_47<-as.data.frame(cor(afq[, c("afqs_48", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_48<-as.data.frame(cor(afq[, c("afqs_49", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_49<-as.data.frame(cor(afq[, c("afqs_50", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_50<-as.data.frame(cor(afq[, c("afqs_51", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_51<-as.data.frame(cor(afq[, c("afqs_52", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_52<-as.data.frame(cor(afq[, c("afqs_53", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_53<-as.data.frame(cor(afq[, c("afqs_54", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_54<-as.data.frame(cor(afq[, c("afqs_55", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_55<-as.data.frame(cor(afq[, c("afqs_56", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_56<-as.data.frame(cor(afq[, c("afqs_57", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_57<-as.data.frame(cor(afq[, c("afqs_58", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_58<-as.data.frame(cor(afq[, c("afqs_59", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_59<-as.data.frame(cor(afq[, c("afqs_60", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_60<-as.data.frame(cor(afq[, c("afqs_61", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_61<-as.data.frame(cor(afq[, c("afqs_62", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_62<-as.data.frame(cor(afq[, c("afqs_63","afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_63<-as.data.frame(cor(afq[, c("afqs_64", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_64<-as.data.frame(cor(afq[, c("afqs_65", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_65<-as.data.frame(cor(afq[, c("afqs_66", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_66<-as.data.frame(cor(afq[, c("afqs_67", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_67<-as.data.frame(cor(afq[, c("afqs_68", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_68<-as.data.frame(cor(afq[, c("afqs_69", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+afq_69<-as.data.frame(cor(afq[, c("afqs_70", "afq_total", "pswq_total", "masq_aa_total")], method = "pearson"))
+
+afq_1<-afq_1[ -c(2:4), ]
+afq_2<-afq_2[ -c(2:4), ]
+afq_3<-afq_3[ -c(2:4), ]
+afq_4<-afq_4[ -c(2:4), ]
+afq_5<-afq_5[ -c(2:4), ]
+afq_6<-afq_6[ -c(2:4), ]
+afq_7<-afq_7[ -c(2:4), ]
+afq_8<-afq_8[ -c(2:4), ]
+afq_9<-afq_9[ -c(2:4), ]
+afq_10<-afq_10[ -c(2:4), ]
+afq_11<-afq_11[ -c(2:4), ]
+afq_12<-afq_12[ -c(2:4), ]
+afq_13<-afq_13[ -c(2:4), ]
+afq_14<-afq_14[ -c(2:4), ]
+afq_15<-afq_15[ -c(2:4), ]
+afq_16<-afq_16[ -c(2:4), ]
+afq_17<-afq_17[ -c(2:4), ]
+afq_18<-afq_18[ -c(2:4), ]
+afq_19<-afq_19[ -c(2:4), ]
+afq_20<-afq_20[ -c(2:4), ]
+afq_21<-afq_21[ -c(2:4), ]
+afq_22<-afq_22[ -c(2:4), ]
+afq_23<-afq_23[ -c(2:4), ]
+afq_24<-afq_24[ -c(2:4), ]
+afq_25<-afq_25[ -c(2:4), ]
+afq_26<-afq_26[ -c(2:4), ]
+afq_27<-afq_27[ -c(2:4), ]
+afq_28<-afq_28[ -c(2:4), ]
+afq_29<-afq_29[ -c(2:4), ]
+afq_30<-afq_30[ -c(2:4), ]
+afq_31<-afq_31[ -c(2:4), ]
+afq_32<-afq_32[ -c(2:4), ]
+afq_33<-afq_33[ -c(2:4), ]
+afq_34<-afq_34[ -c(2:4), ]
+afq_35<-afq_35[ -c(2:4), ]
+afq_36<-afq_36[ -c(2:4), ]
+afq_37<-afq_37[ -c(2:4), ]
+afq_38<-afq_38[ -c(2:4), ]
+afq_39<-afq_39[ -c(2:4), ]
+afq_40<-afq_40[ -c(2:4), ]
+afq_41<-afq_41[ -c(2:4), ]
+afq_42<-afq_42[ -c(2:4), ]
+afq_43<-afq_43[ -c(2:4), ]
+afq_44<-afq_44[ -c(2:4), ]
+afq_45<-afq_45[ -c(2:4), ]
+afq_46<-afq_46[ -c(2:4), ]
+afq_47<-afq_47[ -c(2:4), ]
+afq_48<-afq_48[ -c(2:4), ]
+afq_49<-afq_49[ -c(2:4), ]
+afq_50<-afq_50[ -c(2:4), ]
+afq_51<-afq_51[ -c(2:4), ]
+afq_52<-afq_52[ -c(2:4), ]
+afq_53<-afq_53[ -c(2:4), ]
+afq_54<-afq_54[ -c(2:4), ]
+afq_55<-afq_55[ -c(2:4), ]
+afq_56<-afq_56[ -c(2:4), ]
+afq_57<-afq_57[ -c(2:4), ]
+afq_58<-afq_58[ -c(2:4), ]
+afq_59<-afq_59[ -c(2:4), ]
+afq_60<-afq_60[ -c(2:4), ]
+afq_61<-afq_61[ -c(2:4), ]
+afq_62<-afq_62[ -c(2:4), ]
+afq_63<-afq_63[ -c(2:4), ]
+afq_64<-afq_64[ -c(2:4), ]
+afq_65<-afq_65[ -c(2:4), ]
+afq_66<-afq_66[ -c(2:4), ]
+afq_67<-afq_67[ -c(2:4), ]
+afq_68<-afq_68[ -c(2:4), ]
+afq_69<-afq_69[ -c(2:4), ]
+
+afq_1<-afq_1 %>%
+  rename(item = afqs_1)
+
+afq_2<-afq_2 %>%
+  rename(item = afqs_2)
+
+afq_3<-afq_3 %>%
+  rename(item = afqs_3)
+
+afq_4<-afq_4 %>%
+  rename(item = afqs_4)
+
+afq_5<-afq_5 %>%
+  rename(item = afqs_5)
+
+afq_6<-afq_6 %>%
+  rename(item = afqs_6)
+
+afq_7<-afq_7 %>%
+  rename(item = afqs_7)
+
+afq_8<-afq_8 %>%
+  rename(item = afqs_8)
+
+afq_9<-afq_9 %>%
+  rename(item = afqs_9)
+
+afq_10<-afq_10 %>%
+  rename(item = afqs_10)
+
+afq_11<-afq_11 %>%
+  rename(item = afqs_11)
+
+afq_12<-afq_12 %>%
+  rename(item = afqs_12)
+
+afq_13<-afq_13 %>%
+  rename(item = afqs_13)
+
+afq_14<-afq_14 %>%
+  rename(item = afqs_14)
+
+afq_15<-afq_15 %>%
+  rename(item = afqs_15)
+
+afq_16<-afq_16 %>%
+  rename(item = afqs_16)
+
+afq_17<-afq_17 %>%
+  rename(item = afqs_17)
+
+afq_18<-afq_18 %>%
+  rename(item = afqs_18)
+
+afq_19<-afq_19 %>%
+  rename(item = afqs_19)
+
+afq_20<-afq_20 %>%
+  rename(item = afqs_20)
+
+afq_21<-afq_21 %>%
+  rename(item = afqs_21)
+
+afq_22<-afq_22 %>%
+  rename(item = afqs_22)
+
+afq_23<-afq_23 %>%
+  rename(item = afqs_23)
+
+afq_24<-afq_24 %>%
+  rename(item = afqs_24)
+
+afq_25<-afq_25 %>%
+  rename(item = afqs_25)
+
+afq_26<-afq_26 %>%
+  rename(item = afqs_26)
+
+afq_27<-afq_27 %>%
+  rename(item = afqs_27)
+
+afq_28<-afq_28 %>%
+  rename(item = afqs_28)
+
+afq_29<-afq_29 %>%
+  rename(item = afqs_30)
+
+afq_30<-afq_30 %>%
+  rename(item = afqs_31)
+
+afq_31<-afq_31 %>%
+  rename(item = afqs_32)
+
+afq_32<-afq_32 %>%
+  rename(item = afqs_33)
+
+afq_33<-afq_33 %>%
+  rename(item = afqs_34)
+
+afq_34<-afq_34 %>%
+  rename(item = afqs_35)
+
+afq_35<-afq_35 %>%
+  rename(item = afqs_36)
+
+afq_36<-afq_36 %>%
+  rename(item = afqs_37)
+
+afq_37<-afq_37 %>%
+  rename(item = afqs_38)
+
+afq_38<-afq_38 %>%
+  rename(item = afqs_39)
+
+afq_39<-afq_39 %>%
+  rename(item = afqs_40)
+
+afq_40<-afq_40 %>%
+  rename(item = afqs_41)
+
+afq_41<-afq_41 %>%
+  rename(item = afqs_42)
+
+afq_42<-afq_42 %>%
+  rename(item = afqs_43)
+
+afq_43<-afq_43 %>%
+  rename(item = afqs_44)
+
+afq_44<-afq_44 %>%
+  rename(item = afqs_45)
+
+afq_45<-afq_45 %>%
+  rename(item = afqs_46)
+
+afq_46<-afq_46 %>%
+  rename(item = afqs_47)
+
+afq_47<-afq_47 %>%
+  rename(item = afqs_48)
+
+afq_48<-afq_48 %>%
+  rename(item = afqs_49)
+
+afq_49<-afq_49 %>%
+  rename(item = afqs_50)
+
+afq_50<-afq_50 %>%
+  rename(item = afqs_51)
+
+afq_51<-afq_51 %>%
+  rename(item = afqs_52)
+
+afq_52<-afq_52 %>%
+  rename(item = afqs_53)
+
+afq_53<-afq_53 %>%
+  rename(item = afqs_54)
+
+afq_54<-afq_54 %>%
+  rename(item = afqs_55)
+
+afq_55<-afq_55 %>%
+  rename(item = afqs_56)
+
+afq_56<-afq_56 %>%
+  rename(item = afqs_57)
+
+afq_57<-afq_57 %>%
+  rename(item = afqs_58)
+
+afq_58<-afq_58 %>%
+  rename(item = afqs_59)
+
+afq_59<-afq_59 %>%
+  rename(item = afqs_60)
+
+afq_60<-afq_60 %>%
+  rename(item = afqs_61)
+
+afq_61<-afq_61 %>%
+  rename(item = afqs_62)
+
+afq_62<-afq_62 %>%
+  rename(item = afqs_63)
+
+afq_63<-afq_63 %>%
+  rename(item = afqs_64)
+
+afq_64<-afq_64 %>%
+  rename(item = afqs_65)
+
+afq_65<-afq_65 %>%
+  rename(item = afqs_66)
+
+afq_66<-afq_66 %>%
+  rename(item = afqs_67)
+
+afq_67<-afq_67 %>%
+  rename(item = afqs_68)
+
+afq_68<-afq_68 %>%
+  rename(item = afqs_69)
+
+afq_69<-afq_69 %>%
+  rename(item = afqs_70)
+afq_cor_table_kmc<- dplyr::bind_rows(afq_1,afq_2,afq_3,afq_4,afq_5,afq_6,afq_7,afq_8,afq_9,afq_10,afq_11,afq_12,afq_13,afq_14,afq_15,afq_16,afq_17,afq_18,afq_19,
+                                 afq_20,afq_21,afq_22,afq_23,afq_24,afq_25,afq_26,afq_27,afq_28,afq_29,afq_30,afq_31,afq_32,afq_33,afq_34,afq_35,afq_36,afq_37,
+                                 afq_38,afq_39,afq_40,afq_41,afq_42,afq_43,afq_44,afq_45,afq_46,afq_47,afq_48,afq_49,afq_50,afq_51,afq_52,afq_53,afq_54,afq_55,
+                                 afq_56,afq_57,afq_58,afq_59,afq_60,afq_61,afq_62,afq_63,afq_64,afq_65,afq_66,afq_67,afq_68,afq_69)
+
+afq_cor_table_kmc<-afq_cor_table_kmc[,-1]
+
+write.csv(afq_cor_table_kmc)
+
+############### ################
+#### Time to cluster, baby ####
+install.packages("plot3D")
+library("plot3D")
+install.packages("scatterplot3d") # Install
+library("scatterplot3d") # load
+
+
+afq_kmc<-read.csv(file.choose())
+
+colnames(afq_kmc)<-(c("item", "afq_total", "masq_aa_total", "pswq_total", "subscale"))
+
+x <- afq_kmc$`MASQ Total`
+y <- afq_kmc$`PSWQ Total`
+z <- afq_kmc$`AFQ Total`
+
+plot3d(pc$scores[,1:3], col=iris$Species)
+
+scatter3D(x, y, z, bty = "g",
+          pch = 20, cex = 2, xlab = "MASQ", ylab = "PSWQ", zlab = "AFQ", main = "3D Scatterplot of Item to Scale Correlations", col.var = afq_kmc$Subscale, col = c("#1B9E77", "#D95F02", "#7570B3"), theta = 15, phi = 20)
+
+s3d <- scatterplot3d(afq_kmc[,2:4], pch = 16, color=colors)
+legend("top", legend = levels(afq_kmc$subscale),
+       col =  c("black", "red", "green"), pch = 16, 
+       inset = 0, xpd = TRUE, horiz = TRUE)
+
+#### Let's start clustering #####
+
+
+### Hierarchical clustering of item correlations ####
+di <- dist(afq_kmc[,2:4], method="euclidean")
+tree <- hclust(di, method="ward")
+afq_kmc$hcluster <- as.factor((cutree(tree, k=3)-2) %% 3 +1)
+# that modulo business just makes the coming table look nicer
+plot(tree, xlab="")
+rect.hclust(tree, k=3, border="red")
+
+#### K Means clustering ####
+library("stats")
+install.packages("factoextra")
+library("factoextra")
+
+
+kmeans(afq_kmc[,2:4], centers = 3, nstart = 30)
+
+
+kmean_calc <- function(df, ...){
+  kmeans(df, scaled = ..., nstart = 30)
+}
